22e9bd49456fb1c12ea4e1aa5fc2095bbdaf5968
Gabriel Setznagl <gabrielembraer3@gmail.com>
2024-06-25 13:57:54 -0300

    Realizando exercicios restantes
    .

--- Esquema de Locação (Exercícios).sql
+++ Esquema de Locação (Exercícios).sql
@@ -1,4 +1,4 @@
-// Exercício um
+-- Exercício 01
 
 select C1.NOME as Cliente, F1.TITULO as Filme, L1.DATA_LOC as DataLocacao, L2.DATA_DEVOL as DataDevolucao
 from cliente C1
@@ -7,14 +7,45 @@ join locacao_filme L2 on L1.COD_LOC = L2.COD_LOC
 join filme F1 on L2.COD_FILME = F1.COD_FILME
 where C1.NOME = 'Deonizio Martins'
 
-// Exercícios dois
+-- Exercício 02
 
 select C.NOME as Cliente, D.NOME as Dependente, DEP.PARENTESCO as Parentesco 
 from cliente C
 join dependente DEP on C.COD_CLI = DEP.COD_CLI
 join cliente D on DEP.COD_DEP = D.COD_CLI;
 
-// Exercicios três
+-- Exercício 03
 
+select F.COD_FILME as idFilme , F.TITULO as Filmes , G.NOME as Generos
+from filme F
+join genero G on F.COD_GEN = G.COD_GEN
+where G.NOME = 'Drama';
 
+-- Exercício 04
+
+select C.COD_CLI as codCliente , C.NOME as nomeCliente , P.NOME
+from cliente C
+join profissao P on C.COD_PROF = P.COD_PROF
+where P.NOME = 'Médico';
+
+-- Exercício 05
+
+select F.COD_FILME as codigoFilme , F.TITULO as Titulo , F.QUANTIDADE 
+	as Estoque, C.NOME as nomeCategoria
+from filme F
+join categoria C on F.COD_CAT = C.COD_CAT
+where C.NOME = 'Clássico' and F.QUANTIDADE > 5;
+
+-- Exercício 06
+
+select A.COD_ATOR as id_Ator , A.NOME as nome_Ator , FA.COD_ATOR, FA.COD_FILME,
+	F.TITULO as TituloFilmes
+from ator A
+join filme_ator FA on A.COD_ATOR = FA.COD_ATOR
+join filme F on FA.COD_FILME = F.COD_FILME
+where F.TITULO = 'A Espera de Um Milagre'
+
+-- Exercício 07
+
+select 
 
