6f66dc7105c76b69eb63b7edab69083cd02840c1
Gabriel Setznagl <gabrielembraer3@gmail.com>
2024-07-02 17:29:36 -0300

    .
    .

--- Esquema de Locação (Exercícios).sql
+++ Esquema de Locação (Exercícios).sql
@@ -137,15 +137,89 @@ order by C.NOME;
  Listar os clientes e os filmes que eles alugaram, ordenados por data de 
 locação.*/
 
+SELECT
+    CLI.COD_CLI,
+    CLI.NOME AS NOME_CLIENTE,
+    F.TITULO AS TITULO_FILME,
+    LF.DATA_LOC
+FROM
+    CLIENTE CLI
+INNER JOIN
+    LOCACAO L ON CLI.COD_CLI = L.COD_CLI
+INNER JOIN
+    LOCACAO_FILME LF ON L.COD_LOC = LF.COD_LOC
+INNER JOIN
+    FILME F ON LF.COD_FILME = F.COD_FILME
+ORDER BY
+    LF.DATA_LOC;
 
+/* Exercício 14 
+Mostrar todos os clientes que moram em uma cidade específica e que 
+alugaram filmes de uma categoria específica.*/
 
+SELECT
+    CLI.COD_CLI,
+    CLI.NOME AS NOME_CLIENTE,
+    ENDER.CIDADE,
+    F.TITULO AS TITULO_FILME,
+    CAT.NOME AS CATEGORIA_FILME
+FROM
+    CLIENTE CLI
+INNER JOIN
+    CLI_ENDERECO CE ON CLI.COD_CLI = CE.COD_CLI
+INNER JOIN
+    ENDERECO ENDER ON CE.COD_END = ENDER.COD_END
+INNER JOIN
+    LOCACAO L ON CLI.COD_CLI = L.COD_CLI
+INNER JOIN
+    LOCACAO_FILME LF ON L.COD_LOC = LF.COD_LOC
+INNER JOIN
+    FILMES F ON LF.COD_FILME = F.COD_FILME
+INNER JOIN
+    CATEGORIA CAT ON F.COD_CAT = CAT.COD_CAT
+WHERE
+    ENDER.CIDADE = 'São Paulo'
+    AND CAT.COD_CAT = 2;
 
+/* Exercício 15 
+Encontrar todos os atores que participaram de pelo menos 5 filmes, listando o 
+nome do ator e o número de filmes em que atuou. (DESAFIO)*/
 
-/* Exercício 14 */
+SELECT
+    A.NOME AS NOME_ATOR,
+    COUNT(FA.COD_FILME) AS NUMERO_FILMES
+FROM
+    ATOR A
+INNER JOIN
+    FILME_ATOR FA ON A.COD_ATOR = FA.COD_ATOR
+GROUP BY
+    A.NOME
+HAVING
+    COUNT(FA.COD_FILME) >= 5
+ORDER BY
+    NUMERO_FILMES DESC;
 
+/* Exercício 16 
+16 - Exibir a quantidade total de filmes alugados por categoria e gênero, incluindo 
+apenas as categorias e gêneros que têm mais de 50 filmes alugados no total 
+(DESAFIO)*/
 
-/* Exercício 15 */
-
-
-/* Exercício 16 */
+SELECT
+    C.NOME AS CATEGORIA,
+    G.NOME AS GENERO,
+    COUNT(LF.COD_FILME) AS QUANTIDADE_TOTAL
+FROM
+    LOCACAO_FILME LF
+INNER JOIN
+    FILMES F ON LF.COD_FILME = F.COD_FILME
+INNER JOIN
+    CATEGORIA C ON F.COD_CAT = C.COD_CAT
+INNER JOIN
+    GENERO G ON F.COD_GEN = G.COD_GEN
+GROUP BY
+    C.NOME, G.NOME
+HAVING
+    COUNT(LF.COD_FILME) > 5
+ORDER BY
+    QUANTIDADE_TOTAL DESC;
 
